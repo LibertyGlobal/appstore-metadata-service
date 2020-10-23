c7da0564699d5f129d3095a73e24c0512b6caa4c
alwi <sir.alwi@gmail.com>
2020-10-23 12:07:41 +0200

    SPARK-27431 Add test for STB cannot see hidden version plus checks for all the details

--- appstore-metadata-service-tests/src/test/groovy/com/lgi/appstore/metadata/api/testing/functional/scenarios/StbApiFTSpec.groovy
+++ appstore-metadata-service-tests/src/test/groovy/com/lgi/appstore/metadata/api/testing/functional/scenarios/StbApiFTSpec.groovy
@@ -49,7 +49,12 @@ import static com.lgi.appstore.metadata.api.testing.functional.framework.model.r
 import static com.lgi.appstore.metadata.api.testing.functional.framework.model.response.ApplicationDetailsPath.extract
 import static com.lgi.appstore.metadata.api.testing.functional.framework.model.response.ApplicationDetailsPath.field
 import static com.lgi.appstore.metadata.api.testing.functional.framework.model.response.PathBase.anyOf
+import static com.lgi.appstore.metadata.api.testing.functional.framework.steps.MaintainerSteps.DEFAULT_DEV_ADDRESS
 import static com.lgi.appstore.metadata.api.testing.functional.framework.steps.MaintainerSteps.DEFAULT_DEV_CODE
+import static com.lgi.appstore.metadata.api.testing.functional.framework.steps.MaintainerSteps.DEFAULT_DEV_EMAIL
+import static com.lgi.appstore.metadata.api.testing.functional.framework.steps.MaintainerSteps.DEFAULT_DEV_HOMEPAGE
+import static com.lgi.appstore.metadata.api.testing.functional.framework.steps.MaintainerSteps.DEFAULT_DEV_NAME
+import static com.lgi.appstore.metadata.api.testing.functional.framework.utils.DataUtils.appKeyFor
 import static com.lgi.appstore.metadata.api.testing.functional.framework.utils.DataUtils.getFieldValueFromApplication
 import static com.lgi.appstore.metadata.api.testing.functional.framework.utils.DataUtils.mapAppsToKeys
 import static com.lgi.appstore.metadata.api.testing.functional.framework.utils.DataUtils.pickRandomCategory
@@ -116,7 +121,7 @@ class StbApiFTSpec extends AsmsSpecBase {
     }
 
     @Unroll
-    def "queries for applications list returns apps corresponding to given filter by #field"() {
+    def "queries for applications list returns apps corresponding to given filter by #queryParam"() {
         given: "2 developers create 3 application: first creates 2 incl. multi-versioned and second only 1"
         def dev2 = "lgi2"
         def dev3 = "lgi3"
@@ -169,7 +174,7 @@ class StbApiFTSpec extends AsmsSpecBase {
         JsonPath jsonBody = response.jsonPath()
         def receivedStatus = response.statusCode()
 
-        then: "it gets positive response"
+        then: "it gets positive response SC_OK"
         receivedStatus == SC_OK
 
         and: "the amount of items is as desired"
@@ -192,7 +197,7 @@ class StbApiFTSpec extends AsmsSpecBase {
 
     @Unroll
     def "developer creates new app and stb view details for #behavior"() {
-        given: "2 developers create 2 applications: first with 2 versions (incl. hidden latest) and second with only one version"
+        given: "developer create 2 applications: first with 2 versions (incl. hidden latest) and second with only one version"
         Application app1v1 = newApplication()
                 .withId(appId).withVersion(v1).build()
         Application app1v2 = newApplication()
@@ -225,39 +230,189 @@ class StbApiFTSpec extends AsmsSpecBase {
         "no fallback to latest that is hidden"         | randId() | "1.0.0"  | "2.0.0"  | false       | appId             || SC_OK        | v1 // hidden version is not taken into the account for STB
         "not existing id"                              | randId() | "10.0.0" | "0.1.0"  | true        | "App3"            || SC_NOT_FOUND | _
         "not existing version"                         | randId() | "10.0.0" | "20.0.0" | true        | appId + ":3.0"    || SC_NOT_FOUND | _
+        "query for specific version that is hidden"    | randId() | "0.1.0"  | "1.0.0"  | false       | appId + ":" + v2  || SC_NOT_FOUND | _ // STB cannot get details of hidden version
     }
 
     @Unroll
-    def "stb can get application details"() {
-        given: "developer creates an application with basic header values set"
+    def "details of each version contain separate information about app requirements, maintainer and all visible versions of the application"() {
+        given: "default developer published application with 3 versions"
         def appId = randId()
-        def fieldName = "SomeAppName"
-        def fieldDescription = "Some Description €\\€\\€\\€\\"
-        def fieldCategory = pickRandomCategory()
-        def fieldType = "someCustomType"
-        def fieldUrl = "url://app.great"
-        def fieldIcon = "//home/alwi/Icon.png"
-        Application app = newApplication()
-                .withId(appId)
-                .withVersion("0.0.1")
-                .withName(fieldName)
-                .withDescription(fieldDescription)
-                .withCategory(fieldCategory)
-                .withType(fieldType)
-                .withUrl(fieldUrl)
-                .withIcon(fieldIcon)
+        def v1 = "1.0.0"
+        def v2 = "1.2.0"
+        def v3 = "1.2.3"
+
+        and: "application has v1 with some metadata"
+        def v1Name = "SomeAppName"
+        def v1Description = "Some Description €\\€\\€\\€\\"
+        def v1Category = pickRandomCategory()
+        def v1Type = "someCustomType"
+        def v1Url = "url://app.great"
+        def v1Icon = "//home/alwi/Icon.png"
+        def v1PlatformArch = "v1PlatformArch"
+        def v1PlatformOs = "v1PlatformOs"
+        def v1PlatformVariant = "v1PlatformVariant"
+        def v1HardwareCache = "v1HardwareCache"
+        def v1HardwareDmips = "v1HardwareDmips"
+        def v1HardwarePersistent = "v1HardwarePersistent"
+        def v1HardwareImage = "v1HardwareImage"
+        def v1HardwareRam = "v1HardwareRam"
+        def v1Dependency1Id = "v1Dependency1Id"
+        def v1Dependency1Version = "v1Dependency1Version"
+        def v1Dependency2Id = "v1Dependency2Id"
+        def v1Dependency2Version = "v1Dependency2Version"
+        def v1Feature1Name = "v1Feature1Name"
+        def v1Feature1Version = "v1Feature1Version"
+        def v1Feature1Required = true
+        def v1Feature2Name = "v1Feature2Name"
+        def v1Feature2Version = "v1Feature2Version"
+        def v1Feature2Required = false
+        Application appV1 = newApplication().withId(appId).withVersion(v1)
+                .withName(v1Name)
+                .withDescription(v1Description)
+                .withCategory(v1Category)
+                .withType(v1Type)
+                .withUrl(v1Url)
+                .withIcon(v1Icon)
+                .withPlatform(v1PlatformArch, v1PlatformOs, v1PlatformVariant)
+                .withHardware(v1HardwareCache, v1HardwareDmips, v1HardwarePersistent, v1HardwareRam, v1HardwareImage)
+                .withDependency(v1Dependency1Id, v1Dependency1Version)
+                .withDependency(v1Dependency2Id, v1Dependency2Version)
+                .withFeature(v1Feature1Name, v1Feature1Version, v1Feature1Required)
+                .withFeature(v1Feature2Name, v1Feature2Version, v1Feature2Required)
                 .build()
-        maintainerSteps.createNewApplication_expectSuccess(DEFAULT_DEV_CODE, app)
 
-        when: "stb gets details of application"
-        JsonPath appDetails = stbSteps.getApplicationDetails_expectSuccess(DEFAULT_DEV_CODE, appId)
+        and: "application has v2 with completely different metadata"
+        def v2Name = "v2Name"
+        def v2Description = "v2Description"
+        def v2Icon = "v2Icon"
+        def v2Type = "v2Type"
+        def v2PlatformArch = "v2PlatformArch"
+        def v2PlatformOs = "v2PlatformOs"
+        def v2PlatformVariant = "v2PlatformVariant"
+        def v2HardwareCache = "v2HardwareCache"
+        def v2HardwareDmips = "v2HardwareDmips"
+        def v2HardwarePersistent = "v2HardwarePersistent"
+        def v2HardwareImage = "v2HardwareImage"
+        def v2HardwareRam = "v2HardwareRam"
+        def v2Dependency1Id = "v2Dependency1Id"
+        def v2Dependency1Version = "v2Dependency1Version"
+        def v2Dependency2Id = "v2Dependency2Id"
+        def v2Dependency2Version = "v2Dependency2Version"
+        def v2Feature1Name = "v2Feature1Name"
+        def v2Feature1Version = "v2Feature1Version"
+        def v2Feature1Required = false
+        def v2Feature2Name = "v2Feature2Name"
+        def v2Feature2Version = "v2Feature2Version"
+        def v2Feature2Required = true
+        Application appV2 = newApplication().withId(appId).withVersion(v2)
+                .withName(v2Name)
+                .withDescription(v2Description)
+                .withIcon(v2Icon)
+                .withType(v2Type)
+                .withPlatform(v2PlatformArch, v2PlatformOs, v2PlatformVariant)
+                .withHardware(v2HardwareCache, v2HardwareDmips, v2HardwarePersistent, v2HardwareRam, v2HardwareImage)
+                .withDependency(v2Dependency1Id, v2Dependency1Version)
+                .withDependency(v2Dependency2Id, v2Dependency2Version)
+                .withFeature(v2Feature1Name, v2Feature1Version, v2Feature1Required)
+                .withFeature(v2Feature2Name, v2Feature2Version, v2Feature2Required)
+                .build()
 
-        then: "application details expose basic header values"
-        extract(FIELD_NAME).from(appDetails) == fieldName
-        extract(FIELD_DESCRIPTION).from(appDetails) == fieldDescription
-        extract(FIELD_CATEGORY).from(appDetails) == String.valueOf(fieldCategory)
-        extract(FIELD_TYPE).from(appDetails) == fieldType
-        extract(FIELD_URL).from(appDetails) == fieldUrl
-        extract(FIELD_ICON).from(appDetails) == fieldIcon
+        and: "application has v3 that is hidden"
+        Application appV3 = newApplication()
+                .withId(appId)
+                .withVersion(v3)
+                .withVisible(false)
+                .build()
+
+        and: "developers creates application in these 3 versions"
+        maintainerSteps.createNewApplication_expectSuccess(DEFAULT_DEV_CODE, appV1)
+        maintainerSteps.createNewApplication_expectSuccess(DEFAULT_DEV_CODE, appV2)
+        maintainerSteps.createNewApplication_expectSuccess(DEFAULT_DEV_CODE, appV3)
+
+        when: "developer gets details of application #v1"
+        JsonPath theBody1 = stbSteps.getApplicationDetails_expectSuccess(appKeyFor(appV1))
+
+        then: "the body exposes requested version details with correct header values"
+        field().header().id().from(theBody1) == appId
+        field().header().version().from(theBody1) == v1
+        extract(FIELD_NAME).from(theBody1) == v1Name
+        extract(FIELD_DESCRIPTION).from(theBody1) == v1Description
+        extract(FIELD_CATEGORY).from(theBody1) == String.valueOf(v1Category)
+        extract(FIELD_TYPE).from(theBody1) == v1Type
+        extract(FIELD_URL).from(theBody1) == v1Url
+        extract(FIELD_ICON).from(theBody1) == v1Icon
+
+        and: "the body exposes maintainer section with his details"
+        field().maintainer().name().from(theBody1) == DEFAULT_DEV_NAME
+        field().maintainer().address().from(theBody1) == DEFAULT_DEV_ADDRESS
+        field().maintainer().homepage().from(theBody1) == DEFAULT_DEV_HOMEPAGE
+        field().maintainer().email().from(theBody1) == DEFAULT_DEV_EMAIL
+
+        and: "the body exposes version section with all versions and visibility information"
+        assertThat(field().versions().from(theBody1)).asList().hasSize(2) // 3rd hidden version should not be exposed
+        field().versions().at(0).version().from(theBody1) == v1
+        field().versions().at(0).visible().from(theBody1) == null // version field should not be exposed
+        field().versions().at(1).version().from(theBody1) == v2
+
+        and: "the body exposes requirements section with dependencies information"
+        assertThat(field().requirements().dependencies().id().from(theBody1)).asList().containsExactlyInAnyOrder(v1Dependency1Id, v1Dependency2Id)
+        assertThat(field().requirements().dependencies().version().from(theBody1)).asList().containsExactlyInAnyOrder(v1Dependency1Version, v1Dependency2Version)
+
+        and: "the body exposes requirements section with features information"
+        assertThat(field().requirements().features().name().from(theBody1)).asList().containsExactlyInAnyOrder(v1Feature1Name, v1Feature2Name)
+        assertThat(field().requirements().features().version().from(theBody1)).asList().containsExactlyInAnyOrder(v1Feature1Version, v1Feature2Version)
+        assertThat(field().requirements().features().required().from(theBody1)).asList().containsExactlyInAnyOrder(v1Feature1Required, v1Feature2Required)
+
+        and: "the body exposes requirements section with hardware information"
+        field().requirements().hardware().cache().from(theBody1) == v1HardwareCache
+        field().requirements().hardware().dmips().from(theBody1) == v1HardwareDmips
+        field().requirements().hardware().image().from(theBody1) == v1HardwareImage
+        field().requirements().hardware().ram().from(theBody1) == v1HardwareRam
+        field().requirements().hardware().persistent().from(theBody1) == v1HardwarePersistent
+
+        and: "the body exposes requirements section with platform information"
+        field().requirements().platform().architecture().from(theBody1) == v1PlatformArch
+        field().requirements().platform().variant().from(theBody1) == v1PlatformVariant
+        field().requirements().platform().os().from(theBody1) == v1PlatformOs
+
+        when: "developer gets details of application #v1"
+        JsonPath theBody2 = stbSteps.getApplicationDetails_expectSuccess(appKeyFor(appV2))
+
+        then: "the body exposes requested version details"
+        field().header().id().from(theBody2) == appId
+        field().header().version().from(theBody2) == v2
+
+        and: "the body exposes maintainer section with his details"
+        field().maintainer().name().from(theBody2) == DEFAULT_DEV_NAME
+        field().maintainer().address().from(theBody2) == DEFAULT_DEV_ADDRESS
+        field().maintainer().homepage().from(theBody2) == DEFAULT_DEV_HOMEPAGE
+        field().maintainer().email().from(theBody2) == DEFAULT_DEV_EMAIL
+
+        and: "the body exposes version section with all versions and visibility information"
+        assertThat(field().versions().from(theBody2)).asList().hasSize(2) // 3rd hidden version should not be exposed
+        field().versions().at(0).version().from(theBody2) == v1
+        field().versions().at(0).visible().from(theBody2) == null // version field should not be exposed
+        field().versions().at(1).version().from(theBody2) == v2
+
+        and: "the body exposes requirements section with dependencies information"
+        assertThat(field().requirements().dependencies().id().from(theBody2)).asList().containsExactlyInAnyOrder(v2Dependency1Id, v2Dependency2Id)
+        assertThat(field().requirements().dependencies().version().from(theBody2)).asList().containsExactlyInAnyOrder(v2Dependency1Version, v2Dependency2Version)
+
+        and: "the body exposes requirements section with features information"
+        assertThat(field().requirements().features().name().from(theBody2)).asList().containsExactlyInAnyOrder(v2Feature1Name, v2Feature2Name)
+        assertThat(field().requirements().features().version().from(theBody2)).asList().containsExactlyInAnyOrder(v2Feature1Version, v2Feature2Version)
+        assertThat(field().requirements().features().required().from(theBody2)).asList().containsExactlyInAnyOrder(v2Feature1Required, v2Feature2Required)
+
+        and: "the body exposes requirements section with hardware information"
+        field().requirements().hardware().cache().from(theBody2) == v2HardwareCache
+        field().requirements().hardware().dmips().from(theBody2) == v2HardwareDmips
+        field().requirements().hardware().image().from(theBody2) == v2HardwareImage
+        field().requirements().hardware().ram().from(theBody2) == v2HardwareRam
+        field().requirements().hardware().persistent().from(theBody2) == v2HardwarePersistent
+
+        and: "the body exposes requirements section with platform information"
+        field().requirements().platform().architecture().from(theBody2) == v2PlatformArch
+        field().requirements().platform().variant().from(theBody2) == v2PlatformVariant
+        field().requirements().platform().os().from(theBody2) == v2PlatformOs
     }
 }
