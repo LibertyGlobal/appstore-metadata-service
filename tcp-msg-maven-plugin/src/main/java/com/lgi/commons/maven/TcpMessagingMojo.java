50b5b33f85e9a5f64f736db2b3f1f3f381826ad2
alewir <aleksander.wirth@gmail.com>
2021-01-11 14:23:37 +0100

    NOTICKET Update license headers in test source files

--- tcp-msg-maven-plugin/src/main/java/com/lgi/commons/maven/TcpMessagingMojo.java
+++ tcp-msg-maven-plugin/src/main/java/com/lgi/commons/maven/TcpMessagingMojo.java
@@ -1,13 +1,14 @@
-package com.lgi.commons.maven;
-
 /*
- * Copyright 2001-2005 The Apache Software Foundation.
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 Liberty Global B.V.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +16,8 @@ package com.lgi.commons.maven;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.lgi.commons.maven;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
